ee0e45e1dd70795a371b2b36585d1eeb26e0660c
MDurbin <mhdurbin@users.noreply.github.com>
2018-01-06 14:00:23 -0700

    Add files via upload
    This is the original setup I have for converting PDF documents to TXT documents by reading the PDF document, then writing to a TXT document. I have plans to build on this program by further filtering the data from the PDF documents and building a database with some of the critical data that will be read.

---
+++ PDFReader.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+public class PDFReader{
+
+	public static void WriteFile (String output) throws IOException {
+
+		String FileName = "C:/Users/Matthew/Documents/D&D/Book of Lost Spells (5E).txt";
+
+		FileWriter fw = new FileWriter(FileName);
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		bw.write(output);
+
+		System.out.println("Done");
+
+		if(bw != null) {
+			bw.close();
+		}
+
+		if(fw != null) {
+			fw.close();
+		}
+
+	}
+
+	public static void main(String args[]) throws IOException {
+
+		//Loading an existing document
+		File file = new File("C:/Users/Matthew/Documents/D&D/Book of Lost Spells (5E).pdf");
+		PDDocument document = PDDocument.load(file);
+
+		//Instantiate PDFTextStripper class
+		PDFTextStripper pdfStripper = new PDFTextStripper();
+
+		//Retrieving text from PDF document
+		String text = pdfStripper.getText(document);
+
+		WriteFile(text);
+
+		//Closing the document
+		document.close();
+
+	}
+}
